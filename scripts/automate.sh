50f0df5e7c02718b8cd4327e66f955788d1281cc
Brett Daley <brett-daley@users.noreply.github.com>
2018-07-11 18:49:07 -0400

    temp

--- scripts/automate.sh
+++ scripts/automate.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 PYTHON_CMD="`which python` run_a3c_atari.py"
-OUTPUT_DIR='output'
+#OUTPUT_DIR='auto_output1'
+OUTPUT_DIR='auto_output2'
 
-ENVS='BreakoutNoFrameskip-v3 PongNoFrameskip-v3 QbertNoFrameskip-v3 SeaquestNoFrameskip-v3'
+#ENVS='BreakoutNoFrameskip-v4 PongNoFrameskip-v4'
+ENVS='QbertNoFrameskip-v4 SeaquestNoFrameskip-v4'
 LAMBDAS='0.6 0.7 0.8 1.0'
 
 if [ -z "$CUDA_VISIBLE_DEVICES" ]; then
@@ -30,15 +32,15 @@ function run () {
 for env in $ENVS; do
     for lambda in $LAMBDAS; do
         cmd="$PYTHON_CMD --env $env --Lambda $lambda --history_len 1"
-        filename="dqn_${env}_1_e${lambda}.txt"
+        filename="a3c_${env}_1_e${lambda}.txt"
         run "$cmd" "$filename"
 
         cmd="$PYTHON_CMD --env $env --Lambda $lambda --history_len 4"
-        filename="dqn_${env}_4_e${lambda}.txt"
+        filename="a3c_${env}_4_e${lambda}.txt"
         run "$cmd" "$filename"
 
         cmd="$PYTHON_CMD --env $env --Lambda $lambda --history_len 4 --recurrent"
-        filename="dqn_${env}_rec_e${lambda}.txt"
+        filename="a3c_${env}_rec_e${lambda}.txt"
         run "$cmd" "$filename"
     done
 done
